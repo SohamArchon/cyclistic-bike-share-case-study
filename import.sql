3d5c9bb8aabb68773ab28c1d6be892c300b887e2
SohamArchon <111231613+SohamArchon@users.noreply.github.com>
2025-12-07 16:33:44 +0530

    Update and rename import.sql to sql/Table_create/import.sql

--- import.sql
+++
@@ -1,6 +0,0 @@
-LOAD DATA LOCAL INFILE 'E:/Future Planing/Case_Studies/Dataset/202412-divvy-tripdata/202412-divvy-tripdata.csv'
-INTO TABLE all_trips
-FIELDS TERMINATED BY ',' 
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 LINES;
